9e969a5c2fe4d900fc9dc568e9c6c2e619d84fba
CodeNoJutsu <bellokhalid74@gmail.com>
2024-09-12 10:50:04 +0100

    Just commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
 			<head>
 				<link
 					rel="stylesheet"
+					
 					href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
 				></link>
 			</head>
